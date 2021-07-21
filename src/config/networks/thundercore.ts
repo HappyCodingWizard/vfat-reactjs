398ab77cc5d82d47ff58054d8234d8f31f7fae86
HappyCodingWizard <svenmortedtopdev@gmail.com>
2021-07-21 17:19:51 -0400

    thundercore updated

--- src/config/networks/thundercore.ts
+++ src/config/networks/thundercore.ts
@@ -1,5 +1,13 @@
 export const tableData = {
-  title: 'ThundeCore Network',
-  heading: ['Pool Provider', 'LP', 'Reward Tokens', 'INFO'],
-  rows: [['Ram Protocol', `<a href="thundercore/ram">Various</a>`, 'RAM', 'https://rammer.finance/']],
-}
+  title: "ThundeCore Network",
+  heading: ["Pool Provider", "LP", "Reward Tokens", "INFO"],
+  rows: [
+    [
+      "Ram Protocol",
+      `<a href="thundercore/ram">Various</a>`,
+      "RAM",
+      "https://rammer.finance/",
+    ],
+    ["TTSwap", `<a href="thundercore/ttswap">Various</a>`, "N/A", "https://ttswap.space/"],
+  ],
+};
