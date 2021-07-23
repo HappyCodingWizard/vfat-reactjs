bed778dc290bdd61375ab6c29edb8ace4b1aa8c0
HappyCodingWizard <svenmortedtopdev@gmail.com>
2021-07-23 13:32:07 -0400

    heco updated

--- src/config/networks/heco.ts
+++ src/config/networks/heco.ts
@@ -3,6 +3,7 @@ export const tableData = {
   "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
   "rows": [
     ["Lavaswap  ",`<a href="heco/lava"      >Various</a>`,"LAVA","https://lavaswap.com"],
+    ["Mousse    ",`<a href="heco/mousse"    >Various</a>`,"MOUSSE  ","https://mousse.finance"],
     ["Mdex      ",`<a href="heco/mdex"      >Various</a>`,"MDX ","https://ht.mdex.com"],
     ["Complus   ",`<a href="heco/complus"   >Various</a>`,"COM ","https://heco.complus.exchange"],
     ["Hecostable",`<a href="heco/hecostable">Various</a>`,"HSS ","https://hecostable.finance"],
@@ -11,6 +12,8 @@ export const tableData = {
     ["Pippi     ",`<a href="heco/pippi"     >Various</a>`,"PIPI","https://app.pippi.finance"],
     ["Nut       ",`<a href="heco/nut"       >Various</a>`,"NUT ","https://nut.money"],
     ["Maki      ",`<a href="heco/maki"      >Various</a>`,"MAKI","https://app.makiswap.com"],
-    ["Elk       ",`<a href="heco/elk"       >Various</a>`,"ELK ","https://app.elk.finance"]
+    ["Elk       ",`<a href="heco/elk"       >Various</a>`,"ELK ","https://app.elk.finance"],
+    ["Ninja     ",`<a href="heco/ninja"     >Various</a>`,"NINJA   ","https://www.heco-ninja.com"],
+    ["Butter    ",`<a href="heco/butter"    >Various</a>`,"BUTTER  ","https://butterswap.me/"]
   ]
 }
