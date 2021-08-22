6fbdf3c562c058c137f6e9a6162c16f899b864e3
HappyCodingWizard <svenmortedtopdev@gmail.com>
2021-08-22 13:31:47 -0400

    footer implement

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,124 +1,100 @@
-import React from "react";
-import {
-  Box,
-  useMediaQuery,
-  Container,
-  Link,
-} from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import cx from "classnames";
+import React from 'react'
+import { Box, useMediaQuery, Container } from '@material-ui/core'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import cx from 'classnames'
 
-import { useIsDarkMode } from "state/user/hooks";
-import { Typography } from "components";
+import { useIsDarkMode } from 'state/user/hooks'
 
-// import LOGO_White from "assets/logo_white.png";
-// import LOGO_Text from "assets/logo_text.png";
+import ICON_DARK from 'assets/icon/feather-moon.svg'
+import ICON_LIGHT from 'assets/icon/metro-sun.svg'
+
+import ICON_TELEGRAM from 'assets/icon/awesome-telegram-plane.svg'
+import ICON_FACEBOOK from 'assets/icon/awesome-facebook-square.svg'
+import ICON_TWITTER from 'assets/icon/awesome-twitter-square.svg'
 
 const useStyles = makeStyles(({ palette, breakpoints }) => ({
-  bg: {
-    background: palette.primary.main,
-    marginTop: "50px",
-    padding: 30,
+  root: {
+    position: 'fixed',
+    bottom: 0,
+    zIndex: 100,
+    width: '100%',
+    padding: '20px 0px',
+    background: palette.background.default
   },
 
-  logo: {
-    paddingLeft: "10px",
-    display: "flex",
-    alignItems: "center",
-    cursor: "pointer",
-    "& img": {
-      padding: "20px 10px",
-    },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
 
-  disclaimerContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
+  switchTheme: {
+    display: 'flex',
+
+    '& > div:first-child': {
+      borderRight: `2px solid ${palette.primary.main}`,
+      borderRightStyle: 'dashed'
+    }
   },
 
-  socialContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-
-    [breakpoints.down("xs")]: {
-      flexDirection: "column",
-    },
+  themeImage: {
+    display: 'flex',
+    '& > img': {
+      cursor: 'pointer',
+      padding: '0px 10px',
+      width: '40px'
+    }
   },
 
-  socialIconLink: {
-    borderRadius: "50%",
-    backgroundColor: palette.primary.dark,
-    padding: "10px",
-    marginRight: "20px",
-    cursor: "pointer",
-    textAlign: "center",
-    transition: "background .2s",
-    fontFamily: "auto",
-
-    "& i": {
-      width: "16px",
-      height: "16px",
-    },
-
-    "&:hover": {
-      boxShadow: "0 0 24px 0 #341f103d",
-    },
+  socialList: {
+    display: 'flex',
+    alignItems: 'center'
   },
-}));
+
+  socialItem: {
+    cursor: 'pointer',
+    padding: '0px 10px',
+    display: 'flex',
+    '& > img': {
+      width: '20px'
+    }
+  }
+}))
 
 const Footer: React.FC = () => {
-  const { breakpoints } = useTheme();
-  const dark = useIsDarkMode();
-  const mobile = useMediaQuery(breakpoints.down("xs"));
-  const classes = useStyles({ dark, mobile });
+  const { breakpoints } = useTheme()
+  const dark = useIsDarkMode()
+  const mobile = useMediaQuery(breakpoints.down('xs'))
+  const classes = useStyles({ dark, mobile })
 
   return (
-    <Box className={cx(classes.bg)}>
+    <Box className={cx(classes.root)}>
       <Container>
-        <Box className={cx(classes.disclaimerContainer)}>
-          <Typography variant="h6">
-            This project has recently been updated.
-          </Typography>
-          <br />
-          <br />
-          <Typography variant="h4">*** Disclaimer ***</Typography>
-          <Typography variant="h6">
-            I am in no way affiliated with the above projects, nor do I endorse
-            them. Please do your own research before investing.
-          </Typography>
-        </Box>
+        <Box className={cx(classes.container)}>
+          <Box className={cx(classes.switchTheme)}>
+            <Box className={cx(classes.themeImage)}>
+              <img src={ICON_DARK} alt='dark' />
+            </Box>
+            <Box className={cx(classes.themeImage)}>
+              <img src={ICON_LIGHT} alt='light' />
+            </Box>
+          </Box>
 
-        <Box mt="30px"></Box>
-
-        <Box className={cx(classes.socialContainer)}>
-          {/* <Box className={cx(classes.logo)}>
-            <img src={LOGO_White} alt="logo" />
-            <img src={LOGO_Text} alt="logo" />
-          </Box> */}
-          <Box>
-            <Link className={cx(classes.socialIconLink)} href="#">
-              <i className="fab fa-twitter"></i>
-            </Link>
-            <Link className={cx(classes.socialIconLink)} href="#">
-              <i className="fab fa-instagram"></i>
-            </Link>
-            <Link className={cx(classes.socialIconLink)} href="#">
-              <i className="fab fa-medium"></i>
-            </Link>
-            <Link className={cx(classes.socialIconLink)} href="#">
-              <i className="fab fa-youtube"></i>
-            </Link>
-            <Link className={cx(classes.socialIconLink)} href="#">
-              <i className="fab fa-linkedin"></i>
-            </Link>
+          <Box className={cx(classes.socialList)}>
+            <Box className={cx(classes.socialItem)}>
+              <img src={ICON_TELEGRAM} alt='telegram' />
+            </Box>
+            <Box className={cx(classes.socialItem)}>
+              <img src={ICON_FACEBOOK} alt='facebook' />
+            </Box>
+            <Box className={cx(classes.socialItem)}>
+              <img src={ICON_TWITTER} alt='twitter' />
+            </Box>
           </Box>
         </Box>
       </Container>
     </Box>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
